8bfbe1b8cfe2aaa82517edb2d302584340a6917c
amyroshn <12@d.d>
2023-10-24 21:36:56 +0200

    changing CLI/task refactoring

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,22 +1,48 @@
 use std::os::unix::net::UnixStream;
-use std::io::Write;
+use std::io::{Read, stdout, Write};
 use rustmaster_core::UNIX_DOMAIN_SOCKET_PATH;
 use std::io::stdin;
+use rustmaster_core::api::Action;
+use rustmaster_core::api::Action::Exit;
 
+//TODO: clean up, handle errors
 fn main() {
-    let mut stream = match UnixStream::connect(UNIX_DOMAIN_SOCKET_PATH) {
-        Ok(unix_stream) => unix_stream,
-        Err(_) => {
-            eprintln!("Can't connect to \"{}\"", UNIX_DOMAIN_SOCKET_PATH);
-            return;
-        }
-    };
-    let mut buffer = String::new();
-    let stdin = stdin();
+    loop {
+        let mut stream = match UnixStream::connect(UNIX_DOMAIN_SOCKET_PATH) {
+            Ok(unix_stream) => unix_stream,
+            Err(_) => {
+                eprintln!("Can't connect to \"{}\"", UNIX_DOMAIN_SOCKET_PATH);
+                return;
+            }
+        };
+        let mut buffer = String::new();
+        let stdin = stdin();
 
-    while stdin.read_line(&mut buffer).is_ok() {
-        let trimmed = buffer.trim();
-        stream.write_all(trimmed.as_bytes()).expect("Can't write in socket");
+        print!("taskmaster> ");
+        stdout().flush().expect("Can't flush stdout");
+
         buffer.clear();
+        stdin.read_line(&mut buffer).expect("Can;t");
+
+        let trimmed = buffer.trim();
+        if trimmed == Exit.to_string() {
+            break;
+        }
+
+        match Action::from(trimmed) {
+            Ok(action) => {
+                let serialized_action = serde_json::to_string(&action).expect("Serialization failed");
+                stream.write_all(serialized_action.as_bytes()).expect("aa");
+
+                let mut response = String::new();
+                stream.read_to_string(&mut response).expect("aaa");
+                println!("{}", response);
+            }
+            Err(err) => {
+                println!("{}", err);
+            }
+        }
     }
 }
+
+    
