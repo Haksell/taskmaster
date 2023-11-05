c0259e87efde873a4d5435f2ce18ad63f374e7c6
Axel Brisse <axel.brisse@gmail.com>
2023-11-05 16:00:20 +0100

    fix color prompt

--- taskmasterctl/taskmasterctl.py
+++ taskmasterctl/taskmasterctl.py
@@ -1,5 +1,4 @@
 # TODO: parse action.rs to avoid code duplication
-# TODO: launch without CLI
 
 import cmd
 from enum import Enum
@@ -20,6 +19,9 @@ BOLD = "\033[1m"
 RED = "\033[91m"
 GREEN = "\033[92m"
 CYAN = "\033[96m"
+
+PROMPT_START_IGNORE = "\001"
+PROMPT_END_IGNORE = "\002"
 
 
 class Argument(Enum):
@@ -58,8 +60,7 @@ def send_to_socket(message):
                 if not part:
                     break
                 response_parts.append(part)
-            response = b"".join(response_parts)
-            response = response.decode().rstrip()
+            response = b"".join(response_parts).decode().rstrip()
             if response:
                 print(response)
             elif message == "Shutdown":
@@ -118,8 +119,7 @@ def input_swallowing_interrupt(_input):
 
 
 class TaskMasterShell(cmd.Cmd):
-    # prompt = f"{BOLD}{CYAN}taskmaster>{RESET} "
-    prompt = f"taskmaster> "
+    prompt = f"{PROMPT_START_IGNORE}{BOLD}{CYAN}{PROMPT_END_IGNORE}taskmaster>{PROMPT_START_IGNORE}{RESET}{PROMPT_END_IGNORE} "
 
     def cmdloop(self, *args, **kwargs):
         old_input_fn = cmd.__builtins__["input"]
@@ -128,14 +128,12 @@ class TaskMasterShell(cmd.Cmd):
             super().cmdloop(*args, **kwargs)
         finally:
             cmd.__builtins__["input"] = old_input_fn
-        try:
-            self.old_completer = readline.get_completer()
-            readline.set_completer(self.complete)
-            readline.parse_and_bind(self.completekey + ": complete")
-            old_delims = readline.get_completer_delims()
-            readline.set_completer_delims(old_delims.replace("-", ""))
-        except ImportError:
-            pass
+
+        self.old_completer = readline.get_completer()
+        readline.set_completer(self.complete)
+        readline.parse_and_bind(self.completekey + ": complete")
+        old_delims = readline.get_completer_delims()
+        readline.set_completer_delims(old_delims.replace("-", ""))
 
     def emptyline(self):
         pass
@@ -154,7 +152,7 @@ class TaskMasterShell(cmd.Cmd):
     do_quit = do_exit
 
     def do_config(self, arg):
-        """config <name> : Get the task configuration in json"""
+        """config <name>: Get the task configuration in json"""
         process(arg, Argument.ONE)
 
     def do_shutdown(self, arg):
@@ -162,19 +160,19 @@ class TaskMasterShell(cmd.Cmd):
         process(arg, Argument.ZERO)
 
     def do_start(self, arg):
-        """start <name> : Start a process"""
+        """start <name>: Start a process"""
         process(arg, Argument.ONE)
 
     def do_stop(self, arg):
-        """stop <name> : Stop a process"""
+        """stop <name>: Stop a process"""
         process(arg, Argument.ONE)
 
     def do_status(self, arg):
-        "status        : Get all process status info\nstatus <name> : Get status for a single process"
+        "status       : Get all process status info\nstatus <name>: Get status for a single process"
         process(arg, Argument.OPTIONAL)
 
     def do_update(self, arg):
-        """update <filename> : Reload the config file and add/remove tasks as necessary"""
+        """update <filename>: Reload the config file and add/remove tasks as necessary"""
         process(arg, Argument.ONE)
 
     def complete_update(self, text, line, *_):
@@ -188,6 +186,6 @@ if __name__ == "__main__":
     top_line = INTRO_CHAR * width
     middle_line = "  WELCOME TO TASKMASTER  ".center(width, INTRO_CHAR)
     TaskMasterShell().cmdloop(
-        f"\n{BOLD}{GREEN}{top_line}\n{middle_line}\n{top_line}{RESET}\n"
+        f"{BOLD}{GREEN}{top_line}\n{middle_line}\n{top_line}{RESET}"
     )
     print("Goodbye.")
